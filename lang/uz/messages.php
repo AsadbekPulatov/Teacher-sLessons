01fefb583da18ecca4a1d7d5a5addc95cf6f2734
Lenovo <asadbek.polatov.02@mail.ru>
2023-03-30 13:51:55 +0500

    heroku deploy

---
+++ lang/uz/messages.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+
+    //  buttons
+    'yes' => "Ha",
+    'no' => "Yo'q",
+    'save' => "Saqlash",
+    'add' => "Qo'shish",
+    'edit' => "Tahrirlash",
+    'close' => "Bekor qilish",
+
+    //   headers and menus
+    "login" => "Kirish",
+    "logout" => "Chiqish",
+    "register" => "Ro'yxatdan o'tish",
+
+    //    auth
+    "email" => "Elektron pochta",
+    "password" => "Parol",
+    "remember_me" => "Meni eslab qol",
+    "forgot_password" => "Parolni unutdingizmi?",
+    "forgot_password_text" => "Parolni tiklash uchun elektron pochta manzilingizni kiriting. Sizga parolni tiklash havolasini yuboramiz.",
+    "reset_password" => "Parolni tiklash",
+
+    //   profile
+    "profile" => "Profil",
+    "profile_information" => "Profil haqida ma'lumot",
+    "profile_info_text" => "Hisobingizning profil ma'lumotlari va elektron pochta manzilini yangilang.",
+    "name" => "Ism",
+    "update_password" => "Parolni yangilash",
+    "update_password_text" => "Hisobingiz xavfsizligini ta'minlash uchun uzoq, tasodifiy paroldan foydalanayotganligiga ishonch hosil qiling.",
+    "current_password" => "Hozirgi parol",
+    "new_password" => "Yangi parol",
+    "confirm_password" => "Parolni tasdiqlang",
+    "delete_account" => "Hisobni o'chirish",
+    "delete_account_text" => "Hisobingiz o'chirilgandan so'ng, uning barcha resurslari va ma'lumotlari butunlay o'chiriladi. Hisobingizni o'chirishdan oldin, saqlamoqchi bo'lgan har qanday ma'lumot yoki ma'lumotni yuklab oling.",
+    "delete_account_confirmation" => "Hisobingizni oʻchirib tashlamoqchimisiz?",
+    "delete_account_confirmation_text" => "Hisobingiz o'chirilgandan so'ng, uning barcha resurslari va ma'lumotlari butunlay o'chiriladi. Hisobingizni butunlay oʻchirib tashlamoqchi ekanligingizni tasdiqlash uchun parolingizni kiriting.",
+
+
+    //    labels
+    'delete_confirmation' => "Ҳақиқатан ҳам бу ёзувни оʻчириб ташламоқчимисиз?",
+    'delete_confirmation_text' => "Агар сиз буни ўчириб ташласангиз, бу ёзувни қайта тиклаш имкони ёъқ!",
+
+
+    //Flash messages
+    'logged_in' => "Тизимга кирилди",
+];
