b1daf634dd29e3c46327feb793e9e3bb35e0cb45
Lenovo <asadbek.polatov.02@mail.ru>
2023-12-19 22:45:02 +0500

    bug fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -383,33 +383,22 @@
         body {
             font-family: 'Nunito', sans-serif;
         }
+        .banner{
+            background-image: url({{ asset('banner2.jpg') }});
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
     </style>
 {{--    <link rel="stylesheet" href="{{ asset('bootstrap-5.0.2-dist/css/bootstrap.css') }}">--}}
 {{--    <script src="{{ asset('bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js') }}"></script>--}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script></head>
 <body class="antialiased">
-<div
-    class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-    {{--    @if (Route::has('login'))--}}
-    {{--        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">--}}
-    {{--            @auth--}}
-    {{--                <a href="{{ url('/dashboard') }}"--}}
-    {{--                   class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>--}}
-    {{--            @else--}}
-    {{--                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>--}}
-
-    {{--                @if (Route::has('register'))--}}
-    {{--                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>--}}
-    {{--                @endif--}}
-    {{--            @endauth--}}
-    {{--        </div>--}}
-    {{--    @endif--}}
+<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0 banner">
     <div>
         <a href="{{ route('login') }}" class="btn btn-info">{{ __("messages.login") }}</a>
         <a href="{{ route('register') }}" class="btn btn-info">{{ __("messages.register") }}</a>
-{{--        <a href="{{ route('register', ['role' => 'teacher']) }}" class="btn btn-info">Men O'qituvchiman</a>--}}
-{{--        <a href="{{ route('register', ['role' => 'student']) }}" class="btn btn-info">Men O'quvchiman</a>--}}
     </div>
 </div>
 </body>
